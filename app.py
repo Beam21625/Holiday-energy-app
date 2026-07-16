cd75da7537ff865c402120b90f04b582f6b92a4d
Beam21625 <sohan21625@gmail.com>
2026-07-16 09:11:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def generate_pdf_html(summary_df, sat_date, sun_date):
     </html>
     '''
 
-uploaded_file = st.file_uploader("เลือกไฟล์ตาราง Excel ดิบ (.xlsx)", type=["xlsx"])
+uploaded_file = st.file_uploader("เลือกไฟล์ Excel (.xlsx)", type=["xlsx"])
 
 if uploaded_file is not None:
     try:
@@ -131,7 +131,7 @@ if uploaded_file is not None:
         HTML(string=html_string).write_pdf(pdf_buffer)
         
         st.download_button(
-            label="📥 ดาวน์โหลดรายงานแบบ PDF (หน้าตาเหมือนรูปที่ 2)",
+            label="📥 ดาวน์โหลดรายงานแบบ PDF ",
             data=pdf_buffer.getvalue(),
             file_name=pdf_filename,
             mime="application/pdf"
